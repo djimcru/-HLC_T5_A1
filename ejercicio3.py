df932d265f77c873e5c1791d1cb68185ba3d032c
djimcru <djimcru0801@g.educaand.es>
2025-01-09 10:12:03 +0100

    'nuevo'

---
+++ ejercicio3.py
@@ -0,0 +1,3 @@
+grados=int(input('Introduce los grados '))
+convertidor=grados*9/5+32
+print('Los grados celsius a fahrenheit= ',convertidor )
---
+++ ejercicio8.py
@@ -0,0 +1,6 @@
+base=int(input('introduce la base '))
+altura=int(input('introduce la altura '))
+precio=int(input('introduce el precio por metro cuadrado '))
+area=base*altura/2
+precioTotal=area*precio
+print('El area ',area, 'precio total= ', precioTotal)
